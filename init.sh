bfce2cfbbc27e101e4830f500ccc7c8f63887d4a
Christian Auby <99520+DesktopMan@users.noreply.github.com>
2026-07-30 21:01:11 +0200

    Added OpenOCD with S3 support to venv
    This will override the system installed and Zephyr SDK bundled OpenOCD.

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+OS_TYPE="$(uname -a)"
+if [[ "$OS_TYPE" == MINGW* || "$OS_TYPE" == MSYS* || "$OS_TYPE" == CYGWIN* ]]; then
+  WINDOWS=1
+else
+  WINDOWS=0
+fi
+
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
   echo "usage: init.sh <SoC family> <Zephyr version> <workspace dir>"
   exit 0
@@ -51,10 +58,41 @@ west zephyr-export
 
 uv pip install $(west packages pip | tr -d '\r')
 
+function install_openocd_esp32 {
+  uv pip install openocd-esp32
+
+  OPENOCD_ESP32_URL="https://github.com/espressif/openocd-esp32/releases/download/v0.12.0-esp32-20260703/openocd-esp32-win64-0.12.0-esp32-20260703.zip"
+  OPENOCD_ESP32_ZIP="$(mktemp).zip"
+  curl -LsSf "$OPENOCD_ESP32_URL" -o "$OPENOCD_ESP32_ZIP"
+  OPENOCD_ESP32_TEMP="$(mktemp -d)"
+  unzip -q "$OPENOCD_ESP32_ZIP" -d "$OPENOCD_ESP32_TEMP"
+  cp -rp "$OPENOCD_ESP32_TEMP/openocd-esp32/share/openocd" .venv/share
+
+  if [ "$WINDOWS" -eq 1 ]; then
+    VENV_BIN=$(cygpath -m "$PWD/.venv/Scripts")
+	BIN_EXT=".exe"
+  else
+	VENV_BIN="$PWD/.venv/bin"
+  fi
+
+  west config alias.debug "debug --openocd \"$VENV_BIN/openocd-esp32$BIN_EXT\""
+  west config alias.debugserver "debugserver --openocd \"$VENV_BIN/openocd-esp32$BIN_EXT\""
+}
+
 if [ "$1" = "espressif-s3" ]; then
+  install_openocd_esp32
   west blobs fetch hal_espressif --allow-regex '.*esp32s3.*'
   west sdk install -t xtensa-espressif_esp32s3_zephyr-elf
   west config --local build.board adafruit_feather_esp32s3_tft/esp32s3/procpu
+
+  cat <<-'EOF' >> external/zephyr/boards/adafruit/feather_esp32s3_tft/support/openocd.cfg
+
+		set hardware-watchpoint-limit 2
+
+		$_TARGETNAME_0 configure -event reset-init {
+		  esp appimage_offset 0x0
+		}
+	EOF
 fi
 
 if [ "$1" = "nordic" ]; then
@@ -68,10 +106,9 @@ if [ "$1" = "st-stm32" ]; then
 fi
 
 mkdir samples
-cp -r external/zephyr/samples/hello_world samples
-cp -r external/zephyr/samples/basic/blinky samples
-cp -r external/zephyr/samples/basic/button samples
+cp -rp external/zephyr/samples/hello_world samples
+cp -rp external/zephyr/samples/basic/blinky samples
+cp -rp external/zephyr/samples/basic/button samples
 
 west build -p always samples/blinky
 rm -r build
-
